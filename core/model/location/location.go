e499db6bb5e410b957a3e662308aa3488afa21cc
Denis Kuchugurov <kepkap.dev@gmail.com>
2022-02-03 16:47:16 +0300

    feat(location): update and query location api

--- core/model/location/location.go
+++ core/model/location/location.go
@@ -1,8 +1,21 @@
 package location
 
-// Location represents geographic coordinates.
-type Location struct {
-	Name      string
-	Longitude float64
-	Latitude  float64
+import (
+	"time"
+)
+
+// QueryLocation represents geographic coordinates of principal relative to querying coordinates.
+type QueryLocation struct {
+	PrincipalID string
+	Longitude   float64
+	Latitude    float64
+	Distance    float64
+	LastSeen    time.Time
+}
+
+// UpdateLocation represents geographic coordinates of principal.
+type UpdateLocation struct {
+	PrincipalID string
+	Longitude   float64
+	Latitude    float64
 }
