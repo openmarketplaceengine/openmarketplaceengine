3405477d33a8532a819fe512219e5b46774b8723
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-02-10 20:15:04 +0500

    middleware: use new logging implementation

--- middleware/loghandler/loghandler.go
+++ middleware/loghandler/loghandler.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/openmarketplaceengine/openmarketplaceengine/log"
-	"go.uber.org/zap"
 )
 
 type LogHandler struct {
@@ -15,11 +14,7 @@ type LogHandler struct {
 func (l *LogHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	l.handler.ServeHTTP(w, r)
-	log.GetLogger().Info("served",
-		zap.String("method", r.Method),
-		zap.String("path", r.URL.Path),
-		zap.Duration("duration", time.Since(start)),
-	)
+	log.Debugf("%s %s %s", r.Method, r.URL.Path, time.Since(start))
 }
 
 func NewLogHandler(handlerToWrap http.Handler) *LogHandler {
