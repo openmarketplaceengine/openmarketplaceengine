8088e7b3af908631350e56cea979e1c8be9f2ec7
Denis Kuchugurov <kepkap.dev@gmail.com>
2022-02-14 18:52:08 +0300

    feat(jdl): pickup entity and transitions

--- core/model/job/step/pickup/pickup.go
+++ core/model/job/step/pickup/pickup.go
@@ -1,38 +1,91 @@
 package pickup
 
 import (
-	"github.com/cocoonspace/fsm"
+	"context"
+	"time"
 )
 
+type State int
+type Event int
+
 const (
-	ReadyEvent fsm.Event = iota
+	New State = iota
+	Ready
+	Completed
+	Cancelled
+)
+
+func (s State) String() string {
+	return [...]string{"New", "Ready", "Completed", "Cancelled"}[s]
+}
+
+const (
+	ReadyEvent Event = iota
 	CompletedEvent
+	CancelledEvent
 )
 
-const (
-	ReadyState fsm.State = iota
-	CompletedState
-)
+func (e Event) String() string {
+	return [...]string{"ReadyEvent", "CompletedEvent", "CancelledEvent"}[e]
+}
 
 type Pickup struct {
-	f *fsm.FSM
+	DriverID        string  `json:",string"`
+	PickupLatitude  float64 `json:",string"`
+	PickupLongitude float64 `json:",string"`
+	PassengerIds    string  `json:",string"`
+	UpdatedAt       string  `json:",string"`
+	State           State   `json:",string"`
 }
 
-func (p *Pickup) Complete() {
-	p.f.Event(CompletedEvent)
-}
-
-func NewPickup(onExit func()) *Pickup {
-	return &Pickup{
-		f: newPickUpFsm(onExit),
+func NewPickup(ctx context.Context, driverID string, latitude, longitude float64) (pickup *Pickup, err error) {
+	pickup = &Pickup{
+		DriverID:        driverID,
+		PickupLatitude:  latitude,
+		PickupLongitude: longitude,
+		UpdatedAt:       time.Now().Format(time.RFC3339Nano),
+		State:           New,
 	}
+
+	err = storage.Store(ctx, *pickup)
+	if err != nil {
+		return
+	}
+	return
 }
 
-func newPickUpFsm(onExit func()) *fsm.FSM {
-	f := fsm.New(ReadyState)
-	f.Transition(fsm.On(CompletedEvent), fsm.Src(ReadyState), fsm.Dst(CompletedState))
-	f.Exit(func(state fsm.State) {
-		onExit()
-	})
-	return f
+func (p *Pickup) updateState(ctx context.Context, state State) error {
+	p.UpdatedAt = time.Now().Format(time.RFC3339Nano)
+	p.State = state
+
+	err := storage.Store(ctx, *p)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Pickup) Ready(ctx context.Context) error {
+	err := checkTransition(p.State, ReadyEvent)
+	if err != nil {
+		return err
+	}
+	return p.updateState(ctx, Ready)
+}
+
+func (p *Pickup) Complete(ctx context.Context) error {
+	err := checkTransition(p.State, CompletedEvent)
+	if err != nil {
+		return err
+	}
+	return p.updateState(ctx, Completed)
+}
+
+func (p *Pickup) Cancel(ctx context.Context) error {
+	err := checkTransition(p.State, CancelledEvent)
+	if err != nil {
+		return err
+	}
+	return p.updateState(ctx, Cancelled)
 }
--- core/model/job/step/pickup/pickup_test.go
+++ core/model/job/step/pickup/pickup_test.go
@@ -2,17 +2,96 @@ package pickup
 
 import (
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/openmarketplaceengine/openmarketplaceengine/cfg"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
 )
 
-func TestPickUp(t *testing.T) {
-	sync := make(chan string)
-	onExit := func() {
-		sync <- "done"
-	}
-	pickup := NewPickup(onExit)
+func TestPickup(t *testing.T) {
+	err := cfg.Load()
+	require.NoError(t, err)
 
-	go pickup.Complete()
-	require.Equal(t, "done", <-sync)
+	storage = newStorage(30 * time.Second)
+
+	ctx := context.Background()
+	driverID := uuid.New().String()
+	require.NoError(t, err)
+
+	t.Run("testRetrieveNil", func(t *testing.T) {
+		testRetrieveNil(t)
+	})
+
+	t.Run("testNew", func(t *testing.T) {
+		testNew(ctx, t, driverID)
+	})
+
+	t.Run("testNewToReady", func(t *testing.T) {
+		testNewToReady(ctx, t, driverID)
+	})
+
+	t.Run("testNewToCancelled", func(t *testing.T) {
+		testNewToCancelled(ctx, t, driverID)
+	})
+}
+
+func testRetrieveNil(t *testing.T) {
+	ctx := context.Background()
+	driverID := uuid.New().String()
+	_, err := storage.Retrieve(ctx, driverID)
+	require.Error(t, err)
+}
+
+func testNew(ctx context.Context, t *testing.T, driverID string) {
+	pickup, err := NewPickup(ctx, driverID, 7, 8)
+	require.NoError(t, err)
+
+	retrieved, err := storage.Retrieve(ctx, pickup.DriverID)
+	require.NoError(t, err)
+	assert.Equal(t, New, pickup.State)
+	assert.Equal(t, pickup.DriverID, retrieved.DriverID)
+	assert.Equal(t, pickup.PickupLatitude, retrieved.PickupLatitude)
+	assert.Equal(t, pickup.PickupLongitude, retrieved.PickupLongitude)
+	assert.Equal(t, pickup.State, retrieved.State)
+	assert.Equal(t, pickup.UpdatedAt, retrieved.UpdatedAt)
+}
+
+func testNewToReady(ctx context.Context, t *testing.T, driverID string) {
+	pickup, err := NewPickup(ctx, driverID, 7, 8)
+	require.NoError(t, err)
+
+	prevState := pickup.State
+	prevUpdatedAt := pickup.UpdatedAt
+	err = pickup.Ready(ctx)
+	require.NoError(t, err)
+
+	ready, err := storage.Retrieve(ctx, pickup.DriverID)
+	require.NoError(t, err)
+	assert.Equal(t, New, prevState)
+	assert.Equal(t, pickup.DriverID, ready.DriverID)
+	assert.Equal(t, Ready, ready.State)
+	prev, err := time.Parse(time.RFC3339Nano, prevUpdatedAt)
+	require.NoError(t, err)
+	last, err := time.Parse(time.RFC3339Nano, ready.UpdatedAt)
+	require.NoError(t, err)
+	assert.Less(t, prev.UnixMilli(), last.UnixMilli())
+}
+
+func testNewToCancelled(ctx context.Context, t *testing.T, driverID string) {
+	pickup, err := NewPickup(ctx, driverID, 7, 8)
+	require.NoError(t, err)
+
+	prevState := pickup.State
+	prevUpdatedAt := pickup.UpdatedAt
+	assert.Equal(t, New, pickup.State)
+	err = pickup.Cancel(ctx)
+	require.Error(t, err)
+
+	retrieved, err := storage.Retrieve(ctx, pickup.DriverID)
+	require.NoError(t, err)
+	assert.Equal(t, prevState, retrieved.State)
+	assert.Equal(t, prevUpdatedAt, retrieved.UpdatedAt)
 }
