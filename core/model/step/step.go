2928d04c30c17429cf5920955f8357330ca7a086
Denis Kuchugurov <kepkap.dev@gmail.com>
2022-02-21 19:56:01 +0300

    feat(domain-model): Step entity moved to step package

---
+++ core/model/step/step.go
@@ -0,0 +1,20 @@
+package step
+
+// Action defines primitive action constituting a job.Job
+type Action int
+
+const (
+	GoToLocation Action = iota
+	Pickup
+	DropOff
+	CollectCache
+	CollectVoucher
+	CallPhone
+)
+
+// Step is a part of Job execution
+// JobID refers to job.Job step belongs to
+type Step struct {
+	JobID  string
+	Action Action
+}
