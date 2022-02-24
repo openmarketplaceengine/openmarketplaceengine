4475fae5f216c609a88dd686ce4dfe6a3cee08f2
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-02-25 00:56:21 +0500

    Revert "dao: fix Time.Scan"
    This reverts commit c6de2a7fe456b36d7a03b22c35026ca96f99e01b.

--- dao/time.go
+++ dao/time.go
@@ -54,7 +54,7 @@ func (t *Time) Scan(src interface{}) error {
 		s := *(*string)(unsafe.Pointer(&val))
 		return t.parse(s)
 	}
-	return fmt.Errorf("invalid Time value: %v", src)
+	return nil
 }
 
 func (t *Time) parse(s string) error {
