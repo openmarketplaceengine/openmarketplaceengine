1c0b21b3619bf4d3e4c48d36f109e5dc42382640
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-03-17 20:40:59 +0500

    dom: add mockFirstName, mockLastName, mockEnum test helpers

--- dom/dom_test.go
+++ dom/dom_test.go
@@ -29,3 +29,44 @@ func mockSpeed() int {
 func mockCoord() float64 {
 	return _rnd.Float64() * 10_000
 }
+
+func mockEnum(last int) int {
+	return _rnd.Intn(last + 1)
+}
+
+//-----------------------------------------------------------------------------
+
+func mockFirstName() string {
+	n := _rnd.Intn(len(names))
+	return names[n][0]
+}
+
+func mockLastName() string {
+	n := _rnd.Intn(len(names))
+	return names[n][1]
+}
+
+//-----------------------------------------------------------------------------
+
+var names = [][2]string{
+	{"Jasmine", "Young"},
+	{"Samantha", "Thomson"},
+	{"Joseph", "Berry"},
+	{"Leah", "Thomson"},
+	{"Christopher", "Slater"},
+	{"Carl", "Paterson"},
+	{"Joan", "Skinner"},
+	{"Caroline", "Parr"},
+	{"Carl", "Peters"},
+	{"Kimberly", "Watson"},
+	{"Adam", "Bell"},
+	{"Matt", "Clarkson"},
+	{"Jan", "Jones"},
+	{"Isaac", "Young"},
+	{"Frank", "Ball"},
+	{"Steven", "Baker"},
+	{"Colin", "Churchill"},
+	{"Jane", "James"},
+	{"Warren", "Piper"},
+	{"Oliver", "Avery"},
+}
