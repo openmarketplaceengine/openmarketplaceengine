21c3725dfa17913c9cc453eee6bedc527e3173f4
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-03-24 20:59:01 +0500

    dom: add worker location helpers

--- dom/location.go
+++ dom/location.go
@@ -2,6 +2,7 @@ package dom
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/openmarketplaceengine/openmarketplaceengine/dao"
 	"gopkg.in/yaml.v2"
@@ -16,6 +17,61 @@ type WorkerLocation struct {
 	Longitude float64 `db:"longitude"`
 	Latitude  float64 `db:"latitude"`
 	Speed     int     `db:"speed"`
+}
+
+//-----------------------------------------------------------------------------
+// Getters
+//-----------------------------------------------------------------------------
+
+func LastWorkerLocation(ctx Context, workerID SUID) (loc Coord, err error) {
+	sql := dao.From(workerLocationTable).
+		Select("longitude").To(&loc.Longitude).
+		Select("latitude").To(&loc.Latitude).
+		Where("worker = ?", workerID).
+		OrderBy("stamp desc").
+		Limit(1)
+	err = sql.QueryOne(ctx)
+	return
+}
+
+//-----------------------------------------------------------------------------
+
+func ListWorkerLocation(ctx Context, workerID SUID, limit int) ([]Coord, error) {
+	sql := dao.From(workerLocationTable).
+		Select("longitude").
+		Select("latitude").
+		Where("worker = ?", workerID).
+		OrderBy("stamp desc").
+		Limit(limit)
+	ary := make([]Coord, 0, limit)
+	err := sql.QueryRows(ctx, func(rows *dao.Rows) error {
+		for rows.Next() {
+			var c Coord
+			if err := rows.Scan(&c.Longitude, &c.Latitude); err != nil {
+				return err
+			}
+			ary = append(ary, c)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return ary, nil
+}
+
+//-----------------------------------------------------------------------------
+// Setters
+//-----------------------------------------------------------------------------
+
+func AddWorkerLocation(ctx Context, workerID SUID, lon float64, lat float64, stamp time.Time, speed int) error {
+	sql := dao.Insert(workerLocationTable).
+		Set("worker", workerID).
+		Set("stamp", stamp).
+		Set("longitude", lon).
+		Set("latitude", lat).
+		Set("speed", speed)
+	return dao.ExecTX(ctx, sql)
 }
 
 //-----------------------------------------------------------------------------
--- dom/location_test.go
+++ dom/location_test.go
@@ -2,6 +2,7 @@ package dom
 
 import (
 	"testing"
+	"time"
 
 	"github.com/openmarketplaceengine/openmarketplaceengine/cfg"
 	"github.com/stretchr/testify/require"
@@ -18,6 +19,48 @@ func TestWorkerLocation_Persist(t *testing.T) {
 
 //-----------------------------------------------------------------------------
 
+func TestAddWorkerLocation(t *testing.T) {
+	WillTest(t, "test", true)
+	loc := genCoord(100)
+	wid := mockUUID("drv")
+	ctx := cfg.Context()
+	for i := 0; i < len(loc); i++ {
+		c := loc[i]
+		err := AddWorkerLocation(ctx, wid, c.Longitude, c.Latitude, time.Now(), mockRange(10, 100))
+		require.NoError(t, err)
+	}
+}
+
+//-----------------------------------------------------------------------------
+
+func TestLastWorkerLocation(t *testing.T) {
+	WillTest(t, "test", false)
+	loc := genCoord(100)
+	wid := mockUUID("drv")
+	ctx := cfg.Context()
+	cor, err := LastWorkerLocation(ctx, wid)
+	require.NoError(t, err)
+	c := loc[len(loc)-1]
+	require.Equal(t, c, cor)
+}
+
+//-----------------------------------------------------------------------------
+
+func TestListWorkerLocation(t *testing.T) {
+	WillTest(t, "test", false)
+	max := 100
+	loc := genCoord(max)
+	wid := mockUUID("drv")
+	ctx := cfg.Context()
+	cor, err := ListWorkerLocation(ctx, wid, max)
+	require.NoError(t, err)
+	for i := 0; i < max; i++ {
+		require.Equal(t, loc[max-i-1], cor[i])
+	}
+}
+
+//-----------------------------------------------------------------------------
+
 func genWorkerLocation() *WorkerLocation {
 	return &WorkerLocation{
 		Worker:    mockUUID(),
@@ -26,4 +69,16 @@ func genWorkerLocation() *WorkerLocation {
 		Latitude:  mockCoord(),
 		Speed:     mockSpeed(),
 	}
+}
+
+//-----------------------------------------------------------------------------
+
+func genCoord(n int) []Coord {
+	ary := make([]Coord, n)
+	for i := 0; i < n; i++ {
+		c := &ary[i]
+		c.Longitude = mockCoord()
+		c.Latitude = mockCoord()
+	}
+	return ary
 }
