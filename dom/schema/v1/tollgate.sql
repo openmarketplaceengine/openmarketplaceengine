6f4e169344b350d0b2d28a44e8ffc3a5ce8fd834
Denis Kuchugurov <kepkap.dev@gmail.com>
2022-04-04 13:31:05 +0300

    feat(tollgate-crossing): review feedback schema changes

--- dom/schema/v1/tollgate.sql
+++ dom/schema/v1/tollgate.sql
@@ -1,9 +1,9 @@
 create table if not exists tollgate
 (
-    id         text not null primary key,
-    name       text,
+    id         text        not null primary key,
+    name       text        not null,
     b_boxes    jsonb,
     gate_line  jsonb,
-    created_at timestamptz,
+    created_at timestamptz not null,
     updated_at timestamptz
 );
--- dom/schema/v1/tollgate_crossing.sql
+++ dom/schema/v1/tollgate_crossing.sql
@@ -1,9 +1,8 @@
 create table if not exists tollgate_crossing
 (
-    id          text not null primary key,
-    tollgate_id text,
-    driver_id   text,
-    crossing    jsonb,
-    created_at  timestamptz,
-    constraint fk_tollgate foreign key (tollgate_id) references tollgate (id) on delete CASCADE
+    id          text        not null primary key,
+    tollgate_id text        not null,
+    driver_id   text        not null,
+    crossing    jsonb       not null,
+    created_at  timestamptz not null
 );
