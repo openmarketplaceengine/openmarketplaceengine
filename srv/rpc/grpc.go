0edb54643c345f4141884db58b7a86cf820ae506
vlkolmakov <98851456+vlkolmakov@users.noreply.github.com>
2022-04-06 14:26:24 +0500

    grpc: add service registration callback handling (#81)

--- srv/rpc/grpc.go
+++ srv/rpc/grpc.go
@@ -17,9 +17,12 @@ import (
 	"google.golang.org/grpc"
 )
 
+type RegFun = func(srv *grpc.Server) error
+
 type GrpcServer struct {
 	lsn net.Listener
 	srv *grpc.Server
+	reg []RegFun
 }
 
 //-----------------------------------------------------------------------------
@@ -32,14 +35,25 @@ func NewGrpcServer() *GrpcServer {
 //-----------------------------------------------------------------------------
 
 func (s *GrpcServer) Boot() (err error) {
+
 	c := cfg.Grpc
+
 	addr := c.Addr()
+
 	s.lsn, err = net.Listen("tcp", addr)
+
 	if err != nil {
 		return
 	}
+
 	log.Infof("GRPC listening on %s", addr)
+
 	s.srv = grpc.NewServer(s.configOptions()...)
+
+	if err = s.runreg(s.reg); err != nil {
+		_ = s.lsn.Close()
+		return
+	}
 
 	controller, err := location.NewController(dao.Reds.StoreClient, dao.Reds.PubSubClient)
 	if err != nil {
@@ -59,6 +73,26 @@ func (s *GrpcServer) Stop() error {
 	}
 	s.srv.GracefulStop()
 	return nil
+}
+
+//-----------------------------------------------------------------------------
+
+// Register adds grpc.Server registration callback that is invoked
+// after server creation.
+func (s *GrpcServer) Register(fn RegFun) {
+	if fn == nil {
+		panic("Register function argument is nil")
+	}
+	s.reg = append(s.reg, fn)
+}
+
+//-----------------------------------------------------------------------------
+
+func (s *GrpcServer) runreg(reg []RegFun) (err error) {
+	for i := 0; i < len(reg) && err == nil; i++ {
+		err = reg[i](s.srv)
+	}
+	return
 }
 
 //-----------------------------------------------------------------------------
