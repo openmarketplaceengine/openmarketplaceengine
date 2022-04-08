8518c03087a85811e7900fb49a6a6e366bd4e25b
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-04-08 13:36:01 +0500

    dao: add SkipNoRows error filter

--- dao/error.go
+++ dao/error.go
@@ -1,6 +1,10 @@
 package dao
 
-import "github.com/jackc/pgconn"
+import (
+	"database/sql"
+
+	"github.com/jackc/pgconn"
+)
 
 const (
 	_uniqueViolation = "23505"
@@ -8,6 +12,15 @@ const (
 
 func ErrUniqueViolation(err error) bool {
 	return matchError(err, _uniqueViolation)
+}
+
+//-----------------------------------------------------------------------------
+
+func SkipNoRows(err error) error {
+	if err == sql.ErrNoRows {
+		return nil
+	}
+	return err
 }
 
 //-----------------------------------------------------------------------------
