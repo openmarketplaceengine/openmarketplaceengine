93a284ab7a98f86b2976a1712e156d395d78eee3
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-04-18 12:29:16 +0500

    srv/htp: add hdr package with common HTTP headers/values

---
+++ srv/htp/hdr/hdr.go
@@ -0,0 +1,45 @@
+// Copyright 2022 The Drivers Cooperative. All rights reserved.
+// Use of this source code is governed by a dual
+// license that can be found in the LICENSE file.
+
+package hdr
+
+//goland:noinspection GoUnusedConst
+const (
+	AcceptEncoding   = "Accept-Encoding"
+	AcceptLanguage   = "Accept-Language"
+	Authorization    = "Authorization"
+	Connection       = "Connection"
+	ContentEncoding  = "Content-Encoding"
+	ContentLength    = "Content-Length"
+	ContentType      = "Content-Type"
+	IfModifiedSince  = "If-Modified-Since"
+	LastModified     = "Last-Modified"
+	Location         = "Location"
+	Server           = "Server"
+	TransferEncoding = "Transfer-Encoding"
+	WwwAuthenticate  = "Www-Authenticate"
+	UserAgent        = "User-Agent"
+)
+
+//goland:noinspection GoUnusedConst
+const (
+	AppJSON     = "application/json; charset=utf-8"
+	AppScript   = "application/javascript; charset=utf-8"
+	TextHTML    = "text/html; charset=utf-8"
+	TextPlain   = "text/plain; charset=utf-8"
+	TextScript  = "text/javascript; charset=utf-8"
+	TextJSON    = "text/json; charset=utf-8"
+	TextXML     = "text/xml; charset=utf-8"
+	FavIconMime = "image/x-icon" // "image/vnd.microsoft.icon"
+	FavIconPath = "/favicon.ico"
+)
+
+//goland:noinspection GoUnusedConst
+const (
+	KeepAlive = "keep-alive"
+	ConnClose = "close"
+	Chunked   = "chunked"
+	Gzip      = "gzip"
+	Loopback  = "127.0.0.1"
+)
