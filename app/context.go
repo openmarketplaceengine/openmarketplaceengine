f95b70f6882ee434d4c7ac287d93c42d48aaf8aa
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-04-22 19:13:49 +0500

    feat(app): initial app module commit

---
+++ app/context.go
@@ -0,0 +1,224 @@
+// Copyright 2022 The Drivers Cooperative. All rights reserved.
+// Use of this source code is governed by a dual
+// license that can be found in the LICENSE file.
+
+package app
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"sync/atomic"
+	"syscall"
+)
+
+// SignalContext interface represents the internal context.Context implementation that
+// hooks to SIGINT, SIGTERM, and SIGHUP operating system signals for graceful closing.
+//
+// SignalContext's methods can be called concurrently multiple times from any goroutine.
+type SignalContext interface {
+	context.Context
+
+	// Stop stops the context by closing the context.Done() channel if necessary.
+	Stop()
+
+	// WaitDone pauses execution of the calling goroutine until the
+	// Context.Done() channel closed.
+	//
+	// Returns immediately if the context has already been stopped.
+	WaitDone()
+
+	// WaitStop pauses execution of the calling goroutine until the SignalContext implementation
+	// finishes all background tasks and transitions to invalid state.
+	//
+	// WaitStop will wait for all AsyncRun tasks to finish execution.
+	WaitStop()
+
+	// AsyncRun executes f asynchronously in a separate goroutine.
+	//
+	// WaitStop will not return until all AsyncRun tasks finish.
+	//
+	// Tip: Use for concurrent shutdown of app services after the context stopped.
+	AsyncRun(f func())
+
+	// Running reports if the context is in the running state.
+	Running() bool
+
+	// Stopped reports if the context stopped.
+	Stopped() bool
+
+	// Invalid indicates that the context finished execution and no longer valid for use.
+	Invalid() bool
+}
+
+const (
+	xrunning = 1
+	xstopped = 2
+	xinvalid = 3
+)
+
+// SignalContext internal implementation.
+type sigctx struct {
+	flag int32
+	wait sync.WaitGroup // stop wait group
+	context.Context
+	stop context.CancelFunc
+	sigs chan os.Signal
+	lock sync.Mutex
+}
+
+var _ctx sigctx
+
+// DebugContext function pointer.
+//
+// Set to a valid format function to enable SignalContext
+// debug information output. Default is nil.
+//
+// See: https://pkg.go.dev/fmt for format specs.
+var DebugContext func(format string, args ...interface{})
+
+const ctxDebugPrefix = "SignalContext"
+
+// Context returns SignalContext singleton.
+//
+// It lazily creates the implementer when called for the first time.
+func Context() SignalContext {
+	return _ctx.context()
+}
+
+// Created reports if the SignalContext singleton initialized.
+func Created() bool {
+	return _ctx.Created()
+}
+
+// Running reports if the SignalContext is in the running state.
+func Running() bool {
+	return _ctx.Running()
+}
+
+// Stopped reports that the SignalContext is stopped.
+func Stopped() bool {
+	return _ctx.Stopped()
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) context() SignalContext {
+	if atomic.LoadInt32(&c.flag) == 0 {
+		c.create()
+	}
+	return c
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) Created() bool {
+	f := atomic.LoadInt32(&c.flag)
+	return f > 0 && f < xinvalid
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) Running() bool {
+	return atomic.LoadInt32(&c.flag) == xrunning
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) Stopped() bool {
+	return atomic.LoadInt32(&c.flag) > xrunning
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) Invalid() bool {
+	return atomic.LoadInt32(&c.flag) == xinvalid
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) Stop() {
+	if c.tryStop() {
+		c.debugf("%s: Stop()\n", ctxDebugPrefix)
+		c.stop()
+	}
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) WaitDone() {
+	if c.Running() {
+		<-c.Done()
+	}
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) WaitStop() {
+	c.wait.Wait()
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) AsyncRun(f func()) {
+	if f == nil {
+		panic("RunAsync func argument is nil")
+	}
+	c.wait.Add(1)
+	go func() {
+		defer c.wait.Done() // user defer to safeguard from f() panic
+		f()
+	}()
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) create() {
+	c.lock.Lock()
+	if c.flag > 0 {
+		c.lock.Unlock()
+		return
+	}
+	defer c.debugf("%s: created\n", ctxDebugPrefix)
+	defer c.lock.Unlock()
+	c.Context, c.stop = context.WithCancel(context.Background())
+	c.sigs = make(chan os.Signal, 1)
+	c.wait.Add(1)
+	signal.Notify(c.sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	go c.waitStop()
+	atomic.StoreInt32(&c.flag, xrunning)
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) tryStop() bool {
+	return atomic.CompareAndSwapInt32(&c.flag, xrunning, xstopped)
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) debugf(format string, args ...interface{}) {
+	if DebugContext != nil {
+		DebugContext(format, args...)
+	}
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *sigctx) waitStop() {
+	select {
+	case sig := <-c.sigs:
+		c.debugf("%s: %s\n", ctxDebugPrefix, sig)
+	case <-c.Done():
+		c.debugf("%s: Done()\n", ctxDebugPrefix)
+	}
+	c.Stop()
+	atomic.StoreInt32(&c.flag, xinvalid)
+	c.wait.Done()
+	// Ignore further signals
+	for {
+		sig := <-c.sigs
+		c.debugf("%s: IGNORE: %s\n", ctxDebugPrefix, sig)
+	}
+}
---
+++ app/context_test.go
@@ -0,0 +1,163 @@
+// Copyright 2022 The Drivers Cooperative. All rights reserved.
+// Use of this source code is governed by a dual
+// license that can be found in the LICENSE file.
+
+package app
+
+import (
+	"context"
+	"flag"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+var ctxTestWait int
+
+func init() {
+	flag.IntVar(&ctxTestWait, "wait", 0, "Wait time in seconds before calling Context.Stop()")
+}
+
+//-----------------------------------------------------------------------------
+
+func TestCtx_WaitDone(t *testing.T) {
+	const waiters = 8
+	var wg sync.WaitGroup
+	wg.Add(waiters)
+	debugContext(t)
+	var c sigctx
+	for i := 0; i < waiters; i++ {
+		go testWaitDone(t, c.context(), &wg, i)
+	}
+	stopAfter(c.context(), &wg)
+}
+
+//-----------------------------------------------------------------------------
+
+func TestCtx_WaitStop(t *testing.T) {
+	const waiters = 6
+	var wg sync.WaitGroup
+	wg.Add(waiters)
+	debugContext(t)
+	var c sigctx
+	for i := 0; i < waiters; i++ {
+		go testWaitStop(t, c.context(), &wg, i)
+	}
+	stopAfter(c.context(), &wg)
+}
+
+//-----------------------------------------------------------------------------
+
+func TestCtx_Background_Done(t *testing.T) {
+	done := checkDoneChan(context.Background())
+	require.Equal(t, false, done)
+}
+
+//-----------------------------------------------------------------------------
+// Test Helpers
+//-----------------------------------------------------------------------------
+
+func stopAfter(c SignalContext, g *sync.WaitGroup) {
+	if testing.Short() || ctxTestWait <= 0 {
+		c.Stop()
+		g.Wait()
+		c.WaitStop()
+		return
+	}
+	tmr := time.AfterFunc(time.Duration(ctxTestWait)*time.Second, c.Stop)
+	g.Wait()
+	c.WaitStop()
+	tmr.Stop()
+}
+
+//-----------------------------------------------------------------------------
+
+func testWaitDone(t testing.TB, c SignalContext, g *sync.WaitGroup, i int) {
+	t.Logf("wait: %d", i)
+	c.WaitDone()
+	g.Done()
+	t.Logf("done: %d", i)
+}
+
+//-----------------------------------------------------------------------------
+
+func testWaitStop(t testing.TB, c SignalContext, g *sync.WaitGroup, i int) {
+	t.Logf("wait: %d", i)
+	c.WaitStop()
+	g.Done()
+	t.Logf("stop: %d", i)
+}
+
+//-----------------------------------------------------------------------------
+
+func debugContext(t *testing.T) {
+	if testing.Verbose() {
+		DebugContext = func(format string, args ...interface{}) {
+			t.Helper()
+			t.Logf(format, args...)
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Benchmarks
+//-----------------------------------------------------------------------------
+
+func BenchmarkContext_Done(b *testing.B) {
+	b.Run("Error", BenchmarkContext_DoneErr)
+	b.Run("Chan", BenchmarkContext_DoneChan)
+	b.Run("Stop", BenchmarkContext_DoneStop)
+}
+
+func BenchmarkContext_DoneErr(b *testing.B) {
+	benchDone(b, checkDoneErr)
+}
+
+func BenchmarkContext_DoneChan(b *testing.B) {
+	benchDone(b, checkDoneChan)
+}
+
+func BenchmarkContext_DoneStop(b *testing.B) {
+	benchDone(b, checkDoneStop)
+}
+
+//-----------------------------------------------------------------------------
+
+func benchDone(b *testing.B, f func(ctx context.Context) bool) {
+	var x sigctx
+	c := x.context()
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			if f(c) {
+				b.FailNow()
+			}
+		}
+	})
+}
+
+//go:noinline
+func checkDoneErr(c context.Context) bool {
+	return c.Err() != nil
+}
+
+//go:noinline
+func checkDoneChan(c context.Context) bool {
+	select {
+	case <-c.Done():
+		return true
+	default:
+		return false
+	}
+}
+
+//go:noinline
+func checkDoneStop(c context.Context) bool {
+	if s, ok := c.(SignalContext); ok {
+		return s.Stopped()
+	}
+	panic("SignalContext required")
+}
