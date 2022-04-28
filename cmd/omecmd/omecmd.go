5e33910b762a40d5c4fee51f4513ae3c0376d4c7
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-04-28 10:12:12 +0500

    feat(cmd): command line tool initial commit

---
+++ cmd/omecmd/omecmd.go
@@ -0,0 +1,11 @@
+// Copyright 2022 The Drivers Cooperative. All rights reserved.
+// Use of this source code is governed by a dual
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("OME command line tool")
+}
