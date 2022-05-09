89faee942decca31dae0f1e16f6e1413ac49a86c
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-05-09 14:50:05 +0500

    feat(dir): add FilePath.BasePath

--- app/dir/file.go
+++ app/dir/file.go
@@ -17,6 +17,12 @@ type FilePath struct {
 func (p *FilePath) Set(fullPath string) {
 	p.FullPath = fullPath
 	p.FileName = filepath.Base(fullPath)
+	if len(p.FileName) == 1 {
+		switch p.FileName[0] {
+		case '.', '/':
+			p.FileName = ""
+		}
+	}
 }
 
 func (p *FilePath) Assign(path string, name string) {
@@ -67,4 +73,10 @@ func (p *FilePath) Encode(src interface{}, perm FileMode, enc func(src interface
 		err = p.Write(buf, perm)
 	}
 	return err
+}
+
+//-----------------------------------------------------------------------------
+
+func (p *FilePath) BasePath() string {
+	return p.FullPath[:len(p.FullPath)-len(p.FileName)]
 }
