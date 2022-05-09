265a14e4dfc3115bd3acbaf79b353694b503a05d
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-05-09 15:13:06 +0500

    feat(cmd): update main package

--- cmd/omecmd/omecmd.go
+++ cmd/omecmd/omecmd.go
@@ -4,8 +4,33 @@
 
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/openmarketplaceengine/openmarketplaceengine/app"
+	"github.com/openmarketplaceengine/openmarketplaceengine/cmd/omecmd/cfg"
+	_ "github.com/openmarketplaceengine/openmarketplaceengine/cmd/omecmd/status"
+)
+
+func init() {
+	setlog()
+}
 
 func main() {
-	fmt.Println("OME command line tool")
+	if len(os.Args) == 1 {
+		println("Missing command line arguments. Use '-h' for help.")
+		os.Exit(2)
+	}
+	cli := app.Client()
+	cli.MustInit(app.Domain, cfg.CfgFile)
+	cfg.MustInit()
+	err := cli.Args.Parse(app.Context(), os.Args[1:])
+	app.Exit(err)
+}
+
+func setlog() {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(0)
+	log.SetPrefix("")
 }
