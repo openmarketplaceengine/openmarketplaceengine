6f867178e0e039c4ae4b9709e1c3d2865c46978a
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-05-09 21:45:56 +0500

    feat(api): uptime gRPC service initial commit

---
+++ api/api.go
@@ -0,0 +1,9 @@
+// Copyright 2022 The Drivers Cooperative. All rights reserved.
+// Use of this source code is governed by a dual
+// license that can be found in the LICENSE file.
+
+package api
+
+import (
+	_ "github.com/openmarketplaceengine/openmarketplaceengine/api/svc/uptime"
+)
