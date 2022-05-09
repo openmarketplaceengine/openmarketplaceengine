42e72f1ae17a9cee7c49adf97858703210cc4c19
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-05-09 15:09:44 +0500

    feat(cmd): job package initial commit

---
+++ cmd/omecmd/job/import.go
@@ -0,0 +1,11 @@
+// Copyright 2022 The Drivers Cooperative. All rights reserved.
+// Use of this source code is governed by a dual
+// license that can be found in the LICENSE file.
+
+package job
+
+import "context"
+
+func Import(ctx context.Context, srv string, arg []string) error {
+	return nil
+}
