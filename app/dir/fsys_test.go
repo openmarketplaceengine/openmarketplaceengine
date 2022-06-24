9ffe1a377990e13bed8b45d6ce8aee439edd3a63
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-06-24 07:28:18 +0500

    test(dir): add FsysPath int prefix parser test

--- app/dir/fsys_test.go
+++ app/dir/fsys_test.go
@@ -5,10 +5,16 @@
 package dir
 
 import (
+	"embed"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+//go:embed testdata/intpfx
+var testIntPfx embed.FS
 
 func TestFsysList_String(t *testing.T) {
 	var list FsysList
@@ -19,4 +25,25 @@ func TestFsysList_String(t *testing.T) {
 	require.Equal(t, `["/path/©"]`, list.String())
 	list.Append(nil, "/path/2", "2", 0)
 	require.Equal(t, "[\n  \"/path/©\",\n  \"/path/2\"\n]", list.String())
+}
+
+//-----------------------------------------------------------------------------
+
+func TestFsysPath_IntPrefix(t *testing.T) {
+	var file FsysPath
+	err := WalkDir(testIntPfx, ".", func(path string, d Entry) error {
+		if d.Type().IsRegular() && strings.HasSuffix(path, ".yaml") {
+			file.Reset(testIntPfx, path, d)
+			pfxInt, _ := file.IntPrefix()
+			var ymlInt int
+			if err := file.ReadYAML(&ymlInt); err != nil {
+				return err
+			}
+			if pfxInt != ymlInt {
+				return fmt.Errorf("Int prefix does not match content: %d <> %d", pfxInt, ymlInt)
+			}
+		}
+		return nil
+	})
+	require.NoError(t, err)
 }
