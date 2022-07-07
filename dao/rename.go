c47f1d6e3c9290ef2b2d6441ad1e59f2cbe2269b
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-07-07 21:14:01 +0500

    feat(dao): add rename table wrapper

---
+++ dao/rename.go
@@ -0,0 +1,9 @@
+// Copyright 2022 The Drivers Cooperative. All rights reserved.
+// Use of this source code is governed by a dual
+// license that can be found in the LICENSE file.
+
+package dao
+
+func RenameTable(oldName string, newName string) Executable {
+	return SQLExecf("alter table if exists %q rename to %q", oldName, newName)
+}
---
+++ dao/rename_test.go
@@ -0,0 +1,26 @@
+// Copyright 2022 The Drivers Cooperative. All rights reserved.
+// Use of this source code is governed by a dual
+// license that can be found in the LICENSE file.
+
+package dao
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRenameTable(t *testing.T) {
+	const (
+		oldName = "rename_old"
+		newName = "rename_new"
+	)
+	WillTest(t, "test")
+	var exec ListExec
+	exec.Append(SQLExecf("create table if not exists %q (id text)", oldName))
+	exec.Append(RenameTable(oldName, newName))
+	exec.Append(DropTable(newName, true))
+	err := ExecTX(context.Background(), &exec)
+	require.NoError(t, err)
+}
