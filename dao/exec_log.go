c7653e6a71c470429f06aab0923d184eab4a7069
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-07-14 18:58:11 +0500

    refactor(dao): move SQL logging function out of loggingExecutor

--- dao/exec_log.go
+++ dao/exec_log.go
@@ -16,32 +16,16 @@ type loggingExecutor struct {
 	opt LogOpt
 }
 
-const _argsIndent = "                                     "
-
-func (e *loggingExecutor) logSQL(verb string, query string, args []interface{}) {
-	if len(args) > 0 {
-		debugf("[%s] %s\n%s[ARGS] %s", verb, query, _argsIndent, jsonArgs(args))
-		return
-	}
-	debugf("[%s] %s", verb, query)
-}
-
-func (e *loggingExecutor) logErr(verb string, query string, args []interface{}, err error) {
-	if len(args) > 0 {
-		errorf("%v\n%s[%s] %s\n%s[ARGS] %s", err, _argsIndent, verb, query, _argsIndent, jsonArgs(args))
-		return
-	}
-	errorf("%v\n%s[%s] %s", err, _argsIndent, verb, query)
-}
+type flog = func(format string, args ...interface{})
 
 func (e *loggingExecutor) ExecContext(ctx Context, query string, args ...interface{}) (Result, error) {
 	const verb = "EXEC"
 	if e.opt.LogSQL() {
-		e.logSQL(verb, query, args)
+		logSQL(debugf, verb, query, args)
 	}
 	res, err := e.exe.ExecContext(ctx, query, args...)
 	if err != nil && e.opt.LogErr() && !ShouldSkipError(ctx, err) {
-		e.logErr(verb, query, args, err)
+		logErr(errorf, verb, query, args, err)
 	}
 	return res, err
 }
@@ -49,11 +33,11 @@ func (e *loggingExecutor) ExecContext(ctx Context, query string, args ...interfa
 func (e *loggingExecutor) QueryContext(ctx Context, query string, args ...interface{}) (*Rows, error) {
 	const verb = "STMT"
 	if e.opt.LogSQL() {
-		e.logSQL(verb, query, args)
+		logSQL(debugf, verb, query, args)
 	}
 	rows, err := e.exe.QueryContext(ctx, query, args...)
 	if err != nil && e.opt.LogErr() && !ShouldSkipError(ctx, err) {
-		e.logErr(verb, query, args, err)
+		logErr(errorf, verb, query, args, err)
 	}
 	return rows, err
 }
@@ -61,7 +45,7 @@ func (e *loggingExecutor) QueryContext(ctx Context, query string, args ...interf
 func (e *loggingExecutor) QueryRowContext(ctx Context, query string, args ...interface{}) *Row {
 	const verb = "STMT"
 	if e.opt.LogSQL() {
-		e.logSQL(verb, query, args)
+		logSQL(debugf, verb, query, args)
 	}
 	return e.exe.QueryRowContext(ctx, query, args...)
 }
@@ -74,4 +58,22 @@ func jsonArgs(args []interface{}) string {
 		return "ARGS JSON ERROR: " + err.Error()
 	}
 	return *(*string)(unsafe.Pointer(&buf))
+}
+
+const _argsIndent = "                                     "
+
+func logSQL(flog flog, verb string, query string, args []interface{}) {
+	if len(args) > 0 {
+		flog("[%s] %s\n%s[ARGS] %s", verb, query, _argsIndent, jsonArgs(args))
+		return
+	}
+	flog("[%s] %s", verb, query)
+}
+
+func logErr(flog flog, verb string, query string, args []interface{}, err error) {
+	if len(args) > 0 {
+		flog("%v\n%s[%s] %s\n%s[ARGS] %s", err, _argsIndent, verb, query, _argsIndent, jsonArgs(args))
+		return
+	}
+	flog("%v\n%s[%s] %s", err, _argsIndent, verb, query)
 }
