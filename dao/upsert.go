b2068c90c22c01f3bf4de993c95ce1099f600099
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-07-14 11:21:10 +0500

    feat(dao): add proper atomic upsert

--- dao/upsert.go
+++ dao/upsert.go
@@ -4,6 +4,11 @@
 
 package dao
 
+import (
+	"fmt"
+	"strings"
+)
+
 type UpsertStatus int
 
 const (
@@ -27,4 +32,152 @@ func Upsert(ctx Context, insert, update func() Executable) (Result, UpsertStatus
 		return sql.Result(), UpsertUpdated, nil
 	}
 	return nil, UpsertUnknown, err
+}
+
+//-----------------------------------------------------------------------------
+// Atomic Upsert
+//-----------------------------------------------------------------------------
+
+type colval struct {
+	col string
+	val interface{}
+}
+
+type AtomicUpsert struct {
+	tbl string
+	sql string
+	key colval
+	col []colval
+	res Result
+}
+
+//-----------------------------------------------------------------------------
+
+func UpsertAtomic(table string) *AtomicUpsert {
+	return &AtomicUpsert{tbl: table, col: make([]colval, 0, 8)}
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) Key(col string, val interface{}) *AtomicUpsert {
+	u.key.col = col
+	u.key.val = val
+	return u
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) Set(col string, val interface{}) *AtomicUpsert {
+	u.col = append(u.col, colval{col, val})
+	return u
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) Put(col string, val interface{}) *AtomicUpsert {
+	for i := 0; i < len(u.col); i++ {
+		if c := &u.col[i]; c.col == col {
+			c.val = val
+			return u
+		}
+	}
+	u.col = append(u.col, colval{col, val})
+	return u
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) build() string {
+	var b strings.Builder
+	b.WriteString("INSERT INTO ")
+	b.WriteString(u.tbl)
+	b.WriteString(" (")
+	b.WriteString(u.key.col)
+	n := len(u.col)
+	for i := 0; i < n; i++ {
+		b.WriteString(", ")
+		b.WriteString(u.col[i].col)
+	}
+	b.WriteString(") VALUES ($1")
+	for i := 0; i < n; i++ {
+		b.WriteString(", ")
+		_, _ = fmt.Fprintf(&b, "$%d", (i + 2))
+	}
+	b.WriteString(") ON CONFLICT (")
+	b.WriteString(u.key.col)
+	b.WriteString(") DO UPDATE SET ")
+	for i := 0; i < n; i++ {
+		if i > 0 {
+			b.WriteString(", ")
+		}
+		_, _ = fmt.Fprintf(&b, "%s = $%d", u.col[i].col, (i + 2))
+	}
+	return b.String()
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) Params() []interface{} {
+	n := len(u.col) + 1
+	p := make([]interface{}, n)
+	p[0] = u.key.val
+	for i := 1; i < n; i++ {
+		p[i] = u.col[i-1].val
+	}
+	return p
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) Clear() {
+	u.sql = ""
+	u.key.clear()
+	u.res = nil
+	if n := len(u.col); n > 0 {
+		for i := 0; i < n; i++ {
+			u.col[i].clear()
+		}
+		u.col = u.col[:0]
+	}
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) Table() string {
+	return u.tbl
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) String() string {
+	if len(u.sql) == 0 {
+		u.sql = u.build()
+	}
+	return u.sql
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) Result() Result {
+	return u.res
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) Execute(ctx Context, exe Executor) (err error) {
+	u.res, err = exe.ExecContext(ctx, u.String(), u.Params()...)
+	return
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *AtomicUpsert) RowsAffected() int64 {
+	return RowsAffected(u.res)
+}
+
+//-----------------------------------------------------------------------------
+
+func (c *colval) clear() {
+	c.col = ""
+	c.val = nil
 }
--- dao/upsert_test.go
+++ dao/upsert_test.go
@@ -6,6 +6,7 @@ package dao
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -23,4 +24,28 @@ func TestUpsert(t *testing.T) {
 	_, ups, err = Upsert(ctx, ins, upd)
 	require.NoError(t, err)
 	require.Equal(t, UpsertUpdated, ups)
+}
+
+//-----------------------------------------------------------------------------
+
+func TestAtomicUpsert(t *testing.T) {
+	const table = "upsert_atomic"
+	ctx := WillTest(t, "test")
+	dropTestTable(t, ctx, table)
+	require.NoError(t, ExecTX(ctx, CreateTable(table, "id int primary key not null", "info text", "stamp timestamptz")))
+	logopt := Pgdb.SetLogOpt(LogAll)
+	ups := UpsertAtomic(table)
+	ups.Key("id", 1).Set("info", "info").Set("stamp", time.Now())
+	require.NoError(t, ExecTX(ctx, ups))
+	require.Equal(t, 1, int(ups.RowsAffected()))
+	ups.Put("info", "upsert")
+	require.NoError(t, ExecTX(ctx, ups))
+	require.Equal(t, 1, int(ups.RowsAffected()))
+	var info string
+	sql := From(table).Select("info").To(&info).Where("id = ?", 1)
+	has, err := sql.QueryOne(ctx)
+	require.NoError(t, err)
+	require.True(t, has)
+	require.Equal(t, "upsert", info)
+	Pgdb.SetLogOpt(logopt)
 }
