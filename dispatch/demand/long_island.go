bc8b74419d430cd55a3a2dd33ef9e922c2e68911
Denis Kuchugurov <kepkap.dev@gmail.com>
2022-09-11 14:44:29 +0300

    feat(dispatch): demand tests locations

---
+++ dispatch/demand/long_island.go
@@ -0,0 +1,48 @@
+package demand
+
+var longIslandDemands = []*Demand{
+	{
+		ID: "long-beach-2-river-head",
+		PickUp: LatLon{
+			Lat: 40.588773,
+			Lon: -73.664954,
+		},
+		DropOff: LatLon{
+			Lat: 40.904901,
+			Lon: -72.743725,
+		},
+	},
+	{
+		ID: "long-beach-2-west-babylon",
+		PickUp: LatLon{
+			Lat: 40.588773,
+			Lon: -73.664954,
+		},
+		DropOff: LatLon{
+			Lat: 40.731495,
+			Lon: -73.354590,
+		},
+	},
+	{
+		ID: "long-beach-2-garden-city",
+		PickUp: LatLon{
+			Lat: 40.588773,
+			Lon: -73.664954,
+		},
+		DropOff: LatLon{
+			Lat: 40.733576,
+			Lon: -73.631995,
+		},
+	},
+	{
+		ID: "long-beach-2-valley-stream",
+		PickUp: LatLon{
+			Lat: 40.588773,
+			Lon: -73.664954,
+		},
+		DropOff: LatLon{
+			Lat: 40.674234,
+			Lon: -73.725378,
+		},
+	},
+}
