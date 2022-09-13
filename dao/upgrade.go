7aa4897337b72131cd87db5fe43357ebe76cdbe0
vlkolmakov <viacheslav.kolmakov@drivers.coop>
2022-09-13 15:35:32 +0500

    feat(dao): upgrade workflow wip

--- dao/upgrade.go
+++ dao/upgrade.go
@@ -5,17 +5,33 @@
 package dao
 
 import (
+	"errors"
+	"fmt"
 	"io/fs"
+	"strings"
 	"time"
 
 	"github.com/openmarketplaceengine/openmarketplaceengine/app/dir"
 )
 
 type Upgrade struct {
+	exec    ListExec
+	stamped time.Time
 	Version int
 	Details string
-	Confirm string
+	Drop    Drop
+	Tables  []*UpgradeTable
 	Disable bool
+	Logonly bool
+	Mustrun bool
+}
+
+type UpgradeTable struct {
+	Name   string
+	Create string
+	Select string
+	exec   ListExec
+	count  int
 }
 
 type vermap = map[int]time.Time
@@ -42,11 +58,36 @@ func (u *Upgrade) Insert(ctx Context) error {
 
 //-----------------------------------------------------------------------------
 
+func (u *Upgrade) Stamp() time.Time {
+	if u.stamped.IsZero() {
+		return time.Now()
+	}
+	return u.stamped
+}
+
+//-----------------------------------------------------------------------------
+
 func (u *Upgrade) insert() Executable {
 	sql := Insert(upgradeTable)
 	sql.Set("id", u.Version)
 	sql.Set("info", u.Details)
-	sql.Set("stamp", time.Now())
+	sql.Set("stamp", u.Stamp())
+	return sql
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *Upgrade) Upsert(ctx Context) error {
+	return ExecTX(ctx, u.upsert())
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *Upgrade) upsert() *AtomicUpsert {
+	sql := UpsertAtomic(upgradeTable)
+	sql.Key("id", u.Version)
+	sql.Set("info", u.Details)
+	sql.Set("stamp", u.Stamp())
 	return sql
 }
 
@@ -99,24 +140,109 @@ func (u *upgradeManager) start(ctx Context) (err error) {
 		return
 	}
 	for i := 0; i < n && err == nil; i++ {
-		err = u.readFile(u.list.Path(i))
+		err = u.readFile(ctx, u.list.Path(i))
 	}
 	return
 }
 
 //-----------------------------------------------------------------------------
 
-func (u *upgradeManager) readFile(fpath *dir.FsysPath) (err error) {
+func (u *upgradeManager) readFile(ctx Context, fpath *dir.FsysPath) (err error) {
 	var upg Upgrade
-	debugf("%s: processing upgrade", fpath.Name)
+	debugf("processing upgrade: %q", fpath.Name)
 	if err = fpath.ReadYAML(&upg); err != nil {
 		return
 	}
 	if upg.Disable {
-		debugf("%s: upgrade is disabled", fpath.Name)
+		debugf("upgrade is disabled: %q", fpath.Name)
 		return
 	}
-	return
+	if stamp, ok := u.vmap[upg.Version]; ok {
+		infof("found schema version %d (upgraded on %s)", upg.Version, stamp.Format(time.RFC822))
+		if !upg.Mustrun && !upg.Logonly {
+			return
+		}
+	}
+	return upg.upgrade(ctx)
+}
+
+//-----------------------------------------------------------------------------
+
+func (u *Upgrade) upgrade(ctx Context) error {
+	if u.Mustrun && !u.stamped.IsZero() {
+		infof("force upgrade to version %d: %q", u.Version, u.Details)
+	} else {
+		infof("upgrading to version %d: %q", u.Version, u.Details)
+	}
+	if u.Logonly {
+		debugf("log only upgrade mode")
+	}
+	if u.Drop.Len() > 0 {
+		u.exec.Append(&u.Drop)
+	}
+	for i := range u.Tables {
+		if tbl := u.Tables[i]; tbl != nil {
+			if err := tbl.upgrade(ctx, u.Version); err != nil {
+				return err
+			}
+			if tbl.exec.Len() > 0 {
+				u.exec.Append(&tbl.exec)
+			}
+		}
+	}
+	u.exec.Append(u.upsert())
+	if u.Logonly {
+		return ExecFlog(ctx, infof, &u.exec)
+	}
+	return ExecTX(ctx, &u.exec)
+}
+
+//-----------------------------------------------------------------------------
+
+func (t *UpgradeTable) validate() error {
+	t.Name = strings.TrimSpace(t.Name)
+	if len(t.Name) == 0 {
+		return errors.New("upgrade table name is empty")
+	}
+	t.Select = strings.TrimSpace(t.Select)
+	if len(t.Select) == 0 {
+		return fmt.Errorf("empty 'select' value for table: %q", t.Name)
+	}
+	t.Create = strings.TrimSpace(t.Create)
+	if len(t.Create) == 0 {
+		return fmt.Errorf("empty 'create' value for table: %q", t.Name)
+	}
+	return nil
+}
+
+//-----------------------------------------------------------------------------
+
+func (t *UpgradeTable) upgrade(ctx Context, ver int) error {
+	if err := t.validate(); err != nil {
+		return err
+	}
+	// count
+	sql := From(t.Name).Count("*").To(&t.count)
+	_, err := sql.QueryOne(SkipUndefErrors(ctx))
+	if err != nil {
+		if ErrUndefinedTable.Is(err) {
+			debugf("skipping upgrade for not found table: %q", t.Name)
+			return nil
+		}
+		return err
+	}
+	infof("upgrading table: %q: to version: %d", t.Name, ver)
+	// verify columns
+	sql = From(t.Name).Select(t.Select).Limit(1)
+	_, err = sql.QueryOne(SkipUndefErrors(ctx))
+	if err != nil {
+		if ErrUndefinedColumn.Is(err) {
+			// check if table already upgraded
+			return nil
+		}
+		return fmt.Errorf("table %q select error: %w", t.Name, err)
+	}
+	return nil
 }
 
 //-----------------------------------------------------------------------------
